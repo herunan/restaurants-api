1438d11d2c2a5386a20a39382c27af62293b502d
Hernan Romero <hernrome@gmail.com>
2020-04-13 10:57:45 +0100

    Mid-way through first API creation. Index, Show

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = policy_scope(Restaurant)
+  end
+
+  def show
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant # For Pundit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [:index, :show]
+    end
+  end
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
